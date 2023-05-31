90bea3bc72e57e52d105394805ee146068e0ce2b
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-05-31 12:14:25 +0300

    :sparkles: Add Lab 02 a) Basic HttpClient.

--- lab01/src/cli/module-info.java
+++ lab01/src/cli/module-info.java
@@ -2,5 +2,7 @@ module cli {
     requires java.logging;
     requires utilities;
     requires mymath;
+    requires java.net.http;
+
     uses random.MyRandom;
 }
--- lab01/src/cli/mypackage/cli/Main.java
+++ lab01/src/cli/mypackage/cli/Main.java
@@ -5,16 +5,40 @@ import random.*;
 import java.util.*;
 import datapackage.Person;
 import mymathpackage.MyMath;
+import java.net.http.*;
+import java.io.*;
+import java.net.*;
 
 public class Main {
     private final static Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String [] args) {
-        logger.log(Level.INFO, MyRandom.getInstance().getRandomWord("Hello", "World")); 
+        // logger.log(Level.INFO, MyRandom.getInstance().getRandomWord("Hello", "World")); 
 
-        List<Person> list = MyRandom.getInstance().getRandomListOfPeopleWithNames("Jack", "Tina", "Paul", "John");
-        list.forEach(System.out::println);
+        // List<Person> list = MyRandom.getInstance().getRandomListOfPeopleWithNames("Jack", "Tina", "Paul", "John");
+        // list.forEach(System.out::println);
 
-        System.out.println(MyMath.abs(-7));
+        // System.out.println(MyMath.abs(-7));
+
+        fetch();
+    }
+
+
+    public static void fetch() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://swapi.dev/api/people/"))
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
