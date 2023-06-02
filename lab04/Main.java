58e29a3926eda27c619240e4bace1a46d10ef7e3
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-06-02 10:19:12 +0300

    :sparkles: Lab 04 a) changed real types to var.

--- lab04/Main.java
+++ lab04/Main.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 class Main {
     public static void main(String [] args) {
-        ArrayList<String> a = new ArrayList<String>();        
-        List<String> b = List.of("a", "b", "c");                
-        String c = "Hello";
+        var a = new ArrayList<String>();        
+        var b = List.of("a", "b", "c");                
+        var c = "Hello";
 
         System.out.println(a.getClass().getName());
         System.out.println(b.getClass().getName());                        
