5bef7e3ecee179925bfc624a075f744e01fd00c0
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2023-06-09 11:57:13 +0300

    :sparkles: Add Lab 07 task 2.

--- lab07/Main.java
+++ lab07/Main.java
@@ -2,8 +2,21 @@
 
 public class Main {    
     public static void main(String [] args) {
-
+        print(get(new Circle()));
+        print(get(new Rectangle()));
     }
+
+
+    public static int get(Drawable drawable) {
+        if(drawable instanceof Circle) {
+            return ((Circle) drawable).radius;
+        } else if (drawable instanceof Rectangle) {
+            return ((Rectangle) drawable).height;
+        } else {
+            throw new RuntimeException();
+        }
+     }
+
 
     public static void print(Object o) {
         System.out.println(o);   
